f5b655b66cf7e5549dc033d2b23a33997176ee20
GorLena <zhura1977@list.ru>
2025-01-31 14:46:29 +0300

    test commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  projectId: 'gvs1pt',
+module.exports = {
+  projectId: "iiey3f",
+  // ...rest of the Cypress project config
+
   e2e: {
+    baseUrl: "https://petstore.swagger.io/v2/",
+    //retries: 2,
+    viewportWidth: 1440,
+    viewportHeight: 900,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+};
